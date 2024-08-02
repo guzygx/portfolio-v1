02923d6c13f5aa767daad944c8da150452367cf6
Guzy GX <gxuzxu@gmail.com>
2024-08-02 13:41:03 +0200

    SSR config (wip)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script>
-  import { page } from '$app/stores';
   import "../app.css";
 </script>
 
 <svelte:head>
     <title>X༙྇҉ ꓨ༙྇҉ H༙྇҉ ꓕ༙྇҉ ꓶ༙྇҉ ⅄༙྇҉ Z༙྇҉ ꓵ༙྇҉ ꓨ༙྇҉ </title>
-    <meta name="Portfolio of digital artist Marie Rossi"/>
+    <meta name="Portfolio of Marie Rossi"/>
 </svelte:head>
 
 <slot />
-
-<style>
-
-</style>
