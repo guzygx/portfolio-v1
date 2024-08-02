4c11e1db01ced86bb63b0bec36eb4d3f3e2d9092
Guzy GX <gxuzxu@gmail.com>
2024-08-02 12:49:53 +0200

    tailwind + favicon

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { page } from '$app/stores';
+  import "../app.css";
+</script>
+
+<svelte:head>
+    <title>X༙྇҉ ꓨ༙྇҉ H༙྇҉ ꓕ༙྇҉ ꓶ༙྇҉ ⅄༙྇҉ Z༙྇҉ ꓵ༙྇҉ ꓨ༙྇҉ </title>
+    <meta name="Portfolio of digital artist Marie Rossi"/>
+</svelte:head>
+
+<slot />
+
+<style>
+
+</style>
